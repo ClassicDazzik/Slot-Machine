752967c844694b44f479ab631429082c25f8287e
Clazzic <nikairik@gmail.com>
2021-01-08 14:46:24 +0200

    Done minor work on code

--- main.js
+++ main.js
@@ -3,5 +3,12 @@ var fruits = ["Apple", "Orange","Pear","Banana","Melon"];
 
 function roll() {
 var roll1 = fruits[Math.floor(Math.random()*fruits.length)];
-console.log(roll1)
+var roll2 = fruits[Math.floor(Math.random()*fruits.length)];
+var roll3 = fruits[Math.floor(Math.random()*fruits.length)];
+
+document.getElementById("roll1").innerHTML = roll1;
+document.getElementById("roll2").innerHTML = roll2;
+document.getElementById("roll3").innerHTML = roll3;
+
+console.log(roll1, roll2, roll3)
 }
